3a6e20b3eb57d10beb2e655e0257e7b0d0295994
kirushkinx <richkittyshit@vk.com>
2025-12-05 18:32:28 +0300

    update LivingEntityRendererMixin for 1.21.8 mappings

--- src/main/java/dev/ugamii/whoareyou/mixin/LivingEntityRendererMixin.java
+++ src/main/java/dev/ugamii/whoareyou/mixin/LivingEntityRendererMixin.java
@@ -11,19 +11,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntityRenderer.class)
 public class LivingEntityRendererMixin {
-	@Inject(
-			at = @At("HEAD"),
-			method = "Lnet/minecraft/client/render/entity/LivingEntityRenderer;hasLabel(Lnet/minecraft/entity/LivingEntity;)Z",
-			cancellable = true
-	)
-	private void viewOwnLabel(LivingEntity ent, CallbackInfoReturnable<Boolean> ci) {
-		if (WhoAreYouConfig.enabledOwnName && ent == MinecraftClient.getInstance().cameraEntity)
-		{
-			ci.setReturnValue(MinecraftClient.isHudEnabled());
+
+	@Inject(at = @At("HEAD"), method = "hasLabel(Lnet/minecraft/entity/LivingEntity;D)Z", cancellable = true)
+	private void viewOwnLabel(LivingEntity ent, double distance, CallbackInfoReturnable<Boolean> cir) {
+		if (WhoAreYouConfig.enabledOwnName && ent == MinecraftClient.getInstance().cameraEntity) {
+			cir.setReturnValue(MinecraftClient.isHudEnabled());
 		}
-		if (WhoAreYouConfig.enabledOtherPlayersName && ent != MinecraftClient.getInstance().cameraEntity)
-		{
-			ci.setReturnValue(MinecraftClient.isHudEnabled());
+		if (WhoAreYouConfig.enabledOtherPlayersName && ent != MinecraftClient.getInstance().cameraEntity) {
+			cir.setReturnValue(MinecraftClient.isHudEnabled());
 		}
 	}
 }
