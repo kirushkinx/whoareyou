8e87cc911b2ecfceb58014f919aaacc14dbcc88b
kirushkinx <richkittyshit@vk.com>
2025-12-05 18:33:32 +0300

    yacl update to 3.8.0

--- src/main/java/dev/ugamii/whoareyou/config/ModMenuIntegration.java
+++ src/main/java/dev/ugamii/whoareyou/config/ModMenuIntegration.java
@@ -2,13 +2,12 @@ package dev.ugamii.whoareyou.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import dev.isxander.yacl.api.ConfigCategory;
-import dev.isxander.yacl.api.Option;
-import dev.isxander.yacl.api.YetAnotherConfigLib;
-import dev.isxander.yacl.gui.controllers.TickBoxController;
+import dev.isxander.yacl3.api.*;
+import dev.isxander.yacl3.api.controller.TickBoxControllerBuilder;
 import net.minecraft.text.Text;
 
 public class ModMenuIntegration implements ModMenuApi {
+
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return getConfigScreen();
@@ -19,23 +18,23 @@ public class ModMenuIntegration implements ModMenuApi {
                 .title(Text.of("Who are you?"))
                 .category(ConfigCategory.createBuilder()
                         .name(Text.translatable("whoareyou.config.nameDisplaying"))
-                        .option(Option.createBuilder(boolean.class)
+                        .option(Option.<Boolean>createBuilder()
                                 .name(Text.translatable("whoareyou.config.nameDisplaying.own"))
                                 .binding(
                                         WhoAreYouConfig.enabledOwnNameDefault,
                                         () -> WhoAreYouConfig.enabledOwnName,
                                         (value) -> WhoAreYouConfig.enabledOwnName = value
                                 )
-                                .controller(TickBoxController::new)
+                                .controller(TickBoxControllerBuilder::create)
                                 .build())
-                        .option(Option.createBuilder(boolean.class)
+                        .option(Option.<Boolean>createBuilder()
                                 .name(Text.translatable("whoareyou.config.nameDisplaying.otherPlayers"))
                                 .binding(
                                         WhoAreYouConfig.enabledOtherPlayersNameDefault,
                                         () -> WhoAreYouConfig.enabledOtherPlayersName,
                                         (value) -> WhoAreYouConfig.enabledOtherPlayersName = value
                                 )
-                                .controller(TickBoxController::new)
+                                .controller(TickBoxControllerBuilder::create)
                                 .build())
                         .build())
                 .save(WhoAreYouConfig::save)
